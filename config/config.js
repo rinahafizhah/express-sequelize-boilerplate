dda7cde59dd7dac76b2ae834212384b959dce479
Rina Hafizhah U <rinahafizhah@gmail.com>
2019-01-22 15:23:23 +0700

    fixing env + config.js

--- config/config.js
+++ config/config.js
@@ -1,12 +1,11 @@
-require("dotenv").config()
+require("dotenv").config();
 module.exports = {
-
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   }
   // "test": {
   //   "username": "root",
@@ -22,4 +21,4 @@ module.exports = {
   //   "host": "127.0.0.1",
   //   "dialect": "mysql"
   // }
-}
+};
